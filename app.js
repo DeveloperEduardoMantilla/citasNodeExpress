6bbf095122555547645bca32cd2f8fbec35663a6
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-07-12 08:54:42 -0500

    feat(Farmacia_Node_Js): :sparkles: EndPoint - Get - Medicos - Filtro
    Se crea EndPoint que permite obtener los medicos filtrados por el id de la especialidad

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import storagePacientes from "./routers/pacientes.js";
 import storageCitas from "./routers/citas.js";
+import storageMedicos from "./routers/medicos.js";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const appExpress = express();
 appExpress.use(express.json());
 appExpress.use("/pacientes", storagePacientes);
 appExpress.use("/citas", storageCitas);
+appExpress.use("/medicos", storageMedicos);
 
 const config = JSON.parse(process.env.MY_CONFIG);
 appExpress.listen(config, ()=>console.log(`http://${config.hostname}:${config.port}`));
